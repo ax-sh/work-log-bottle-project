70ed836410f04c1e61ec39900ddee248c37d9ceb
Axmin <>
2020-12-30 11:30:12 +0545

    add login form

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,5 +1,21 @@
 const LoginPage = () => {
-  return <div>{"Login"}</div>;
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(e.target);
+  };
+  return (
+    <div>
+      <form id="login-form" onSubmit={onSubmit}>
+        <div>
+          <input type="text" placeholder="username" />
+        </div>
+        <div>
+          <input type="password" placeholder="password" />
+          <input type="submit" value="Login" />
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default LoginPage;
