7c12b6835e749a9497e7a5a440b1eeeeab646b01
Axmin <>
2020-12-30 20:20:47 +0545

    add heroku as backend

--- src/api/index.js
+++ src/api/index.js
@@ -10,19 +10,23 @@ const employees = names.map((i, n) => ({
 }));
 
 const api = {
+  DOMAIN: "https://bottle-work-log-api-project.herokuapp.com",
+  // DOMAIN: "http://localhost:3001",
   getAllEmployees() {
     return employees;
   },
   getLogsForEmployee(id) {
+    // just for fast prototyping purposes
+    // ALWAYS sanitize code for security
     return axios
-      .get("http://localhost:3001/logs?employeeId=" + id)
+      .get(this.DOMAIN + "/logs?employeeId=" + id)
       .then((x) => x.data);
   },
   createLog(log) {
-    return axios.post("http://localhost:3001/logs", log);
+    return axios.post(this.DOMAIN + "/logs", log);
   },
   getLogs() {
-    return axios.get("http://localhost:3001/logs").then((x) => x.data);
+    return axios.get(this.DOMAIN + "/logs").then((x) => x.data);
   },
 };
 
