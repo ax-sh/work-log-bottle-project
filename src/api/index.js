c431860be96db6128244e1472ca8fd94b52ea271
Axmin <>
2020-12-30 09:00:39 +0545

    add create log method on api

--- src/api/index.js
+++ src/api/index.js
@@ -1,23 +1,23 @@
+import axios from "axios";
 
 const names = ["Olivia", "Emma", "Ava", "Sophia", "Isabella"];
-const admins = [0]
+const admins = [0];
 const employees = names.map((i, n) => ({
   name: i,
-  id:n,
+  id: n,
   image: `https://randomuser.me/api/portraits/women/${n}.jpg`,
-  isAdmin:admins.indexOf(n)>-1
+  isAdmin: admins.indexOf(n) > -1,
 }));
 
 const api = {
-    getAllEmployees(){
-        return employees
-    },
-    getEmployee(id){
+  getAllEmployees() {
+    return employees;
+  },
+  getEmployee(id) {},
+  getEmployeeLog(id) {},
+  createLog(log) {
+    return axios.post("http://localhost:3001/logs", log);
+  },
+};
 
-    },
-    getEmployeeLog(id){
-
-    }
-}
-
-export default api
+export default api;
