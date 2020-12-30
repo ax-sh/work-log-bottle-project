5f1dcc3af7620d39714e614a47f66011c3d45860
Axmin <>
2020-12-30 10:23:20 +0545

    added the 3 time interval selectors

--- src/pages/ReportPage.js
+++ src/pages/ReportPage.js
@@ -1,7 +1,29 @@
-const ReportPage = ()=>{
-    return <div>
-        {"Report"}
-    </div>
-}
+import { useSelector, useDispatch } from "react-redux";
+import React from "react";
 
-export default ReportPage
+const ReportPage = () => {
+  const state = useSelector((state) => state);
+  const dispatch = useDispatch();
+  const [selected, setSelected] = React.useState("1D");
+
+  const onClick = ({ target }) => {
+    setSelected(target.value);
+    console.log({ selected });
+  };
+
+  return (
+    <div>
+      <div className="time-interval-container">
+        <input type="button" value="1D" onClick={onClick} />
+        <input type="button" value="1W" onClick={onClick} />
+        <input type="button" value="1M" onClick={onClick} />
+      </div>
+      {/* {JSON.stringify(state.logs)} */}
+      {selected === "1D" && <div>1D</div>}
+      {selected === "1W" && <div>1W</div>}
+      {selected === "1M" && <div>1M</div>}
+    </div>
+  );
+};
+
+export default ReportPage;
